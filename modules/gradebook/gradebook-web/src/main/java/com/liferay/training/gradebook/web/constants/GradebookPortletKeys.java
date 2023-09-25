d40c1c765793d26f460d951b887b9783ee8121c7
Thiago Buarque <thiago.buarque@liferay.com>
2023-09-25 19:08:26 -0300

    now it works :D

--- modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/constants/GradebookPortletKeys.java
+++ modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/constants/GradebookPortletKeys.java
@@ -6,6 +6,6 @@ package com.liferay.training.gradebook.web.constants;
 public class GradebookPortletKeys {
 
 	public static final String GRADEBOOK =
-		"com_liferay_training_gradebook_web_portletap";
+		"com_liferay_training_gradebook_web_portlet_GradebookPortlet";
 
 }
